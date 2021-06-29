72c427072b4bb7cb5208664e6677b58f22d77afa
Jonas Jerndin <jonas@queenslab.se>
2021-06-29 22:08:58 +0200

    Some API stuff

---
+++ src/dotnet-api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace BuyScout.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IHubContext<TestHub> _hubContext;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(
+            IHubContext<TestHub> hubContext,
+            ILogger<WeatherForecastController> logger)
+        {
+            _hubContext = hubContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            _logger.LogInformation("Called Get at {Timestamp}", DateTime.Now);
+
+            await _hubContext.Clients.All.SendCoreAsync("Broadcast", new []
+            {
+                "WeatherForecast",
+                $"Called Get at {DateTime.Now}"
+            });
+
+            await _hubContext.Clients.All.SendCoreAsync("WeatherForecastRequested", new []
+            {
+                "WeatherForecast",
+                $"Called Get at {DateTime.Now}"
+            });
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ src/dotnet-api/Startup.cs
@@ -0,0 +1,104 @@
+using System.Threading.Tasks;
+using BuyScout.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+namespace BuyScout.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BuyScout API", Version = "v1" });
+            });
+            
+            services.AddSignalR();
+
+            services.AddCors(options => {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+            });
+
+            services.AddHostedService<EmailHostedService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuyScout API v1"));
+            }
+
+            if(env.IsProduction())
+            {
+                app.UseHttpsRedirection();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("AllowAll");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseWebSockets();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<TestHub>("/hubs/testHub");
+            });
+        }
+    }
+
+    public interface ITestHubClient
+    {
+        Task ReceiveMessage(string user, string message);
+
+        Task Broadcast(string user, string message);
+    }
+
+    public class TestHub : Hub<ITestHubClient>
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.ReceiveMessage(user, message);
+        }
+
+        public async Task Broadcast(string user, string message)
+        {
+            await Clients.All.Broadcast(user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "All Connected Users");
+            await base.OnConnectedAsync();
+        }
+    }
+}
